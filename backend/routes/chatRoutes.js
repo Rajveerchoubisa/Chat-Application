5fb28309f163dc04e9f31581035dc8b29830b556
Rajveer Choubisa <ranveer0596@gmail.com>
2025-02-07 12:00:41 +0530

    Chat route,controller

---
+++ backend/routes/chatRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+const router = express.Router();
+import {protect} from '../middleware/authMiddleware.js'
+import { accessChat } from '../controllers/chatControllers.js';
+
+ router.route("/").post(protect,accessChat);
+// router.route("/").get(protect,fetchChats);
+// router.route("/group").post(protect,createGroupChat);
+// router.route("/rename").put(protect,renameGroup);
+// router.route("/groupremove").put(protect,removeFromGroup);
+// router.route("/groupadd").put(protect,addToGroup);
+
+export default router;
