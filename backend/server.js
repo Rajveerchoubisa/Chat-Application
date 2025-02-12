6f73b74223dbadb80c4590e71eb6598658e92524
Rajveer Choubisa <ranveer0596@gmail.com>
2025-02-12 18:40:05 +0530

    ....

--- backend/server.js
+++ backend/server.js
@@ -29,11 +29,11 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 if(process.env.NODE_ENV === 'productions'){
 
-  app.use(express.static(path.join(__dirname1,'/frontend/build')))
+  app.use(express.static(path.join(__dirname1,'/frontend/dist')))
 
-    app.get('*',(req,res) => {
-      res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"))
-    });
+    app.get('*',(req,res) => 
+      res.sendFile(path.resolve(__dirname1,"frontend","dist","index.html"))
+    );
 }else {
   app.get("/", (req, res) => {
     res.send("Api is running ");
