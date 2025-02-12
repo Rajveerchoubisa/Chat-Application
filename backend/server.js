e8aef2a7e527102327f849d4096a6f3ece73e104
Rajveer Choubisa <ranveer0596@gmail.com>
2025-02-12 18:28:49 +0530

    ............

--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
-app.use(cors());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Api is running ");
-});
+// app.get("/", (req, res) => {
+//   res.send("Api is running ");
+// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
@@ -48,7 +46,7 @@ if(process.env.NODE_ENV === 'production'){
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, console.log(`server started on port ${PORT}`));
 
